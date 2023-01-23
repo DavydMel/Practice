0251eab7f6c0ebccf777febbe73577666f60cc5b
Davyd Melnychenko <ipz204_mdv@student.ztu.edu.ua>
2023-01-23 15:23:42 +0200

    added dropdowns

--- Week1/scripts/dropdown.js
+++ Week1/scripts/dropdown.js
@@ -1,26 +1,54 @@
 const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach(dropdown => {
-   const select = dropdown.querySelector(".dropdown__select");
-   const options = dropdown.querySelectorAll(".dropdown__list li");
-   const selected = dropdown.querySelector(".dropdown__selected");
+    const list = dropdown.querySelector(".dropdown__list");
+    let active;
 
-   select.addEventListener("click", () => {
-      dropdown.classList.toggle("active");
-      document.addEventListener("click", closeByOuterClick);
-   });
+    function refreshList() {
+        active = list.querySelector(".dropdown__active");
+        active.addEventListener("click", () => {
+            dropdown.classList.toggle("active");
+            document.addEventListener("click", closeByOuterClick);
+        });
+    }
 
-   options.forEach(option => {
-      option.addEventListener("click", () => {
-          selected.innerText = option.innerText;
-          dropdown.classList.remove("active");
-          options.forEach(option => {
-              option.classList.remove("dropdown__active");
-          })
-          option.classList.add("dropdown__active");
-          document.removeEventListener("click", closeByOuterClick);
-      }) ;
-   });
+    function ChoseNewActive(e) {
+        active.classList.remove("dropdown__active");
+        let tmp = active.cloneNode(true);
+        tmp.addEventListener("click", ChoseNewActive);
+        list.replaceChild(tmp, active);
+
+        let li = e.currentTarget;
+        tmp = li.cloneNode(true);
+        tmp.classList.add("dropdown__active");
+        list.replaceChild(tmp, li);
+
+        dropdown.classList.remove("active");
+
+        refreshList();
+    }
+
+    refreshList();
+
+    let liElements = list.querySelectorAll("li");
+    liElements.forEach(li => {
+        if (!li.classList.contains("dropdown__active")) {
+            li.addEventListener("click", ChoseNewActive);
+        }
+    });
+
+
+    // options.forEach(option => {
+    //    option.addEventListener("click", () => {
+    //        selected.innerText = option.innerText;
+    //        dropdown.classList.remove("active");
+    //        options.forEach(option => {
+    //            option.classList.remove("dropdown__active");
+    //        })
+    //        option.classList.add("dropdown__active");
+    //        document.removeEventListener("click", closeByOuterClick);
+    //    }) ;
+    // });
 
     function closeByOuterClick(e) {
         if (e.target !== dropdown && !dropdown.contains(e.target)) {
